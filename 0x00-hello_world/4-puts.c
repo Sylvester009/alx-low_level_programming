5758bd91bc2424713d2286818bc13417f03c6a6a
Sylvester <samsylvester09@gmail.com>
2023-08-10 02:20:37 -0700

    Hello,puts third update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main : Entry point
- * puts : Write string in stdo
- * return : 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
