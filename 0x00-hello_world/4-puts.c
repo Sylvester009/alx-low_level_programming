98b9a4f3e0409957f0311bec84d3dd427739d88c
Sylvester <samsylvester09@gmail.com>
2023-08-10 02:17:01 -0700

    Hello, puts updated again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main : Entry point
+ * puts : Write string in stdo
+ * return : 0 (success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
