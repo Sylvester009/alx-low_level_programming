7a54fb815159507c7656b8ccf2daa6f457992f60
Sylvester <samsylvester09@gmail.com>
2023-08-11 14:26:19 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print digit of base 10 starting from 0
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 78; i < 88; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
