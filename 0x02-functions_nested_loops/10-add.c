8b23195179f5dece7e29ab24e91e53b6e0b060f8
Sylvester <samsylvester09@gmail.com>
2023-08-16 07:17:23 -0700

    a + b and 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: one of the number to be added
+ * @b: the second number to be added
+ * Return: value of two integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
