f88cb08f87c496463036205f6092e968909a1b92
Sylvester <samsylvester09@gmail.com>
2023-08-16 05:52:42 -0700

    absolute value and last digit of number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @c: Number to be computed
+ * Return: absolute value of integer or zero
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
