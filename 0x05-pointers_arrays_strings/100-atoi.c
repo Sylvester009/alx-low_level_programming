fca8d162e319f302716bfff27a3a066c8f65e431
Sylvester <samsylvester09@gmail.com>
2023-08-21 17:04:45 -0700

    string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string to an integer
+ * @s: integer to br converted
+ * Return: (0);
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned int j = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[i])
+	{
+		if (s[i] == 45)
+		{
+			min *= -1;
+		}
+		while (s[i] >= 48 && s[i] <= 57)
+		{
+			isi = 1;
+			j = (j * 10)  + (s[i] - '\0');
+			b++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	j *= min;
+	return (j);
+}
+
