3437a6c627cf1ccb13d449a01e7861ed2828f41e
Sylvester <samsylvester09@gmail.com>
2023-08-24 16:39:19 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @str: pointer to string parameter
+ * Return: *str
+ */
+
+char *rot13(char *str)
+{
+	int i;
+	int x;
+
+	char alp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char alprot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (x = 0; x < 52; x++)
+		{
+			if (str[i] == alp[x])
+			{
+				str[i] == alprot[x];
+				break;
+			}
+		}
+	}
+	return (str);
+}
