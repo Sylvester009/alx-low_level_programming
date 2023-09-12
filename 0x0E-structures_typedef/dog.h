fd1e552e39683093fa648e4469cc224e4f4ba353
Sylvester <samsylvester09@gmail.com>
2023-09-12 07:09:34 -0700

    c structures 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,17 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *str);
+void free_dog(dog_t *d);
+
 #endif
