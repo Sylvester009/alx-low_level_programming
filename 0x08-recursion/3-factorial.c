3c5385869ab3fd354f897c830f3b60eddcb3f137
Sylvester009 <samsylvester09@gmail.com>
2023-10-25 05:14:06 -0700

    function that prints a factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * factorial - returns the factorial of a given number.
+ * @n: number to be factorised
+ *
+ * Return: factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
