cbb4b392eef902b2d68f1d12722a178fa9d83797
Sylvester009 <samsylvester09@gmail.com>
2023-10-27 14:13:40 -0700

    making changes

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * main - prints numbers of coins to make change
+ * for an amount of money
+ *
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ * Return: 0 success, 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, i, cent;
+
+	char coin_value[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	result = 0;
+
+	cent = atoi(argv[1]);
+
+	if (cent < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < 5 && cent >= 0; i++)
+	{
+		while (cent >= coin_value[i])
+		{
+			result++;
+
+			cent -= coin_value[i];
+		}
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
