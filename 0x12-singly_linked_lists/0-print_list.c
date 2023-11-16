5c7e83b50859738d0e1dce70c5217c0f0c94b7b7
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-16 23:49:04 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,10 +7,13 @@
  * Return: the number of nodes
  */
 
-size_t print_list(const list_t *h) {
+size_t print_list(const list_t *h)
+{
 size_t node = 0;
-while (h != NULL) {
-if (h->str == NULL) {
+while (h != NULL)
+{
+if (h->str == NULL)
+{
 printf("[0] (nil)\n");
 }
 else
@@ -20,6 +23,5 @@ printf("[%u] %s\n", h->len, h->str);
 node++;
 h = h->next;
 }
-
-    return (node);
+return (node);
 }
