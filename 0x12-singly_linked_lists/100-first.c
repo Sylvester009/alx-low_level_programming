53f373e386b593dd3b38dd3d2ab18e4db1598d15
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-17 00:37:32 +0100

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* meassage - prints a sentence before the main
+* Return: void
+*/
+
+void message(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
