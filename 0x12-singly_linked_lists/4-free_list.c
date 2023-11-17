a93931df11caff51dbde9960393033b9c8e11471
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-17 01:35:32 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,12 +8,11 @@
 
 void free_list(list_t *head)
 {
-list_t *temp;
+list_t *temp = head;
 while (head != NULL)
 {
-temp = head->next;
-free(head->str);
-free(head);
-head = temp;
+head = head->next;
+free(temp->str);
+free(temp);
 }
 }
