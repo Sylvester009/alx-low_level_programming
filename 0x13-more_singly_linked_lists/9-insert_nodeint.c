47b48c1eb13f72e9348f5c3535e0fb620961ee0b
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-21 14:05:46 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,46 +3,46 @@
 /**
 * insert_nodeint_at_index - inserts a new node at a given position.
 * @head: pointer to the first node in the list
-* @idx: index where the new node shuold be added
+* @idx: index where the new node should be added
 * @n: data to insert in the new node
 *
-* Return:  the address of the new node, or NULL if it failed
+* Return: the address of the new node, or NULL if it failed
 */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int i;
-listint_t *nextz;
+listint_t *new_node;
 listint_t *current = *head;
+
+if (!head)
+return (NULL);
+ 
+*new_node = malloc(sizeof(listint_t));
+if (!new_node)
+return (NULL);
+
+new_node->n = n;
 
 if (idx == 0)
 {
-nextz = malloc(sizeof(struct listint_t));
-if (nextz == NULL)
-{
-return (NULL);
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
-nextz->n = n;
-nextz->next = *head;
-*head = nextz;
 
-return (nextz);
-}
-else if (*head == NULL)
+for (i = 0; current && i < idx - 1; i++)
 {
-return (NULL);
-}
-for (i = 0; current && i < idx; i++)
-{
-if (i == idx - 1)
-{
-nextz->next = current->next;
-current->next = nextz;
-return (nextz);
-}
-else
 current = current->next;
 }
-
+if (!current)
+{
+free(new_node);
 return (NULL);
+}
+
+new_node->next = current->next;
+current->next = new_node;
+
+return (new_node);
 }
