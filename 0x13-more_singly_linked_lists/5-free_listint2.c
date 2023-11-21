0bd9f7cb0c49eacac46796e697c8ac70bb631385
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-21 13:23:20 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,16 @@
 
 void free_listint2(listint_t **head)
 {
-listint_t *temp;
+listint_t *nextz;
+
+if (head == NULL)
+return;
 
 while (*head != NULL)
 {
-temp = (*head)->next;
+nextz = (*head)->next;
 free(*head);
-*head = temp;
+*head = nextz;
 }
 *head = NULL;
 }
