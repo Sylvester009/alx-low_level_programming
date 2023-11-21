221adf17e83538e7de3710c815b59699a35a6d71
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-21 12:40:45 +0100

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all the elements of a listint_t list.
+* @h: linked list of type listint_t
+
+* Return: number of nodes
+*/
+
+size_t print_listint(const struct listint_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->data);
+h = h->next;
+count++;
+}
+return (count);
+}
