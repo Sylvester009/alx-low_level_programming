b4aa2b21cb0b3820a2c5d488319fa8945f2c4907
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-24 00:03:46 +0100

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_binary - prints the binary representation of a number.
+* @n: number to be printed in binary
+* Return: void
+*/
+
+void print_binary(unsigned long int n)
+{
+int i, num = 0;
+unsigned long int nextz;
+
+for (i = 63; i >= 0; i--)
+{
+nextz = n >> i;
+
+if (nextz & 1)
+{
+_putchar('1');
+num++;
+}
+else if (num != NULL)
+_putchar('0');
+}
+if (num == NULL)
+_putchar('0');
+}
