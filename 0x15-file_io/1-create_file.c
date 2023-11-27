11bb9e43e0cd7178f41b8e3187aa99855ca4c542
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-27 23:28:02 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,17 +16,20 @@ if (!filename)
 {
 		return (-1);
 }
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-if (fd == -1 || written == -1)
-{
-		return (-1);
-}
+	
 if (text_content != NULL)
 {
 for (length = 0; text_content[length];)
 	length++;
 }
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	written = write(fd, text_content, length);
+	
+if (fd == -1 || written == -1)
+{
+		return (-1);
+}
+	
 close(fd);
 
 	return (1);
