7976e79e6239fa82fe05a946ccd2a2d387476fb8
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-15 22:55:58 +0100

    Create 0-strcat.c

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @src: string to be appended
+ * @dest: string that is appended into
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int dlength = 0, i;
+
+	while (dest[dlength])
+	{
+		dlength++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlength] = src[i];
+		dlength++;
+	}
+	dest[dlength] = '\0';
+	return (dest);
+}
