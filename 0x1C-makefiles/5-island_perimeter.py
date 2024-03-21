557301f0b66072e2575b34ee442152b071eb1bca
Samuel Sylvester <samsylvester09@gmail.com>
2024-03-21 04:00:04 +0100

    Create 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+Island Perimeter Module
+"""
+
+
+def island_perimeter(grid):
+    """
+    function that returns the perimeter of the island
+    described in grid
+
+    Parameters:
+    - grid: a list of list of integers
+            0 represents a water zone.
+            1 represents a land zone.
+
+    Returns:
+    - Perimeter of the island.
+    """
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(row):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+              
+        # Check neighboring cells and minus 1 for each adj land cell
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < row - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
